d701b9bb73ef0fdf4ed50a61856416c13dc9d197
Robin <robin59530@gmail.com>
2024-02-17 07:45:45 -0500

    init server.js, ajout ligne de comande pour start les 2 projet

--- Hunt/App.tsx
+++ Hunt/App.tsx
@@ -6,7 +6,7 @@ import SignUpScreen from "./view/sign/signUp/SignUpScreen";
 export default function App() {
   return (
     <View style={styles.container}>
-      <SignInScreen />
+      <SignUpScreen />
     </View>
   );
 }
