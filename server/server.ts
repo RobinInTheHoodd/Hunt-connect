15f356d98cfa026a0134a823b6ea4df77ed4f14f
Robin <robin59530@gmail.com>
2024-02-25 11:33:36 -0500

    Modification lancement seulement test ou dev

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import verifyTokenMiddleware from "./middleware/authMiddleware";
 import router from "./routes/index";
 import errorPostgresMiddleware from "./middleware/errorPostgresMiddleware";
 import errorFirebaseMiddleware from "./middleware/errorFirebaseMiddleware";
+
 require("dotenv").config();
 
 const app = express();
@@ -19,8 +20,10 @@ app.use(router);
 app.use(errorPostgresMiddleware);
 app.use(errorFirebaseMiddleware);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
 
 export default app;
