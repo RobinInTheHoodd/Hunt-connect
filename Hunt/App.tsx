47018bdae5dbcad757a9b6aa6537c71b543477e7
Robin <robin59530@gmail.com>
2024-02-28 19:04:35 -0500

    Modification ajoutant react-navigation / changement potentiel pour expo-router ?

--- Hunt/App.tsx
+++ Hunt/App.tsx
@@ -2,12 +2,23 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import SignInScreen from "./view/sign/signIn/SignInScreen";
 import SignUpScreen from "./view/sign/signUp/SignUpScreen";
+import { AuthProvider } from "./utils/context/authContext";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { registerRootComponent } from "expo";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SignUpScreen />
-    </View>
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
 
@@ -16,3 +27,5 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+registerRootComponent(App);
