a021b26eb00b6cfa38a7afe781a10be5fc76400f
Robin <robin59530@gmail.com>
2024-02-28 19:59:09 -0500

    Ajout de class registerRequest et mapper to userContect de firebase

---
+++ server/models/auth/RegisterRequest.ts
@@ -0,0 +1,72 @@
+import { UserRecord } from "firebase-admin/auth";
+
+export interface IRegisterRequest {
+  UUID?: string;
+  display_name?: string;
+  email: string;
+  phone?: string;
+  password?: string;
+  role: number;
+  hut_name?: string;
+  hut_number?: string;
+  create_at?: Date;
+  last_update?: Date;
+}
+
+export default class RegisterRequest implements IRegisterRequest {
+  UUID?: string;
+  display_name?: string;
+  email: string;
+  phone?: string;
+  password?: string;
+  role: number;
+  hut_name?: string;
+  hut_number?: string;
+  create_at?: Date;
+  last_update?: Date;
+
+  constructor({
+    UUID,
+    display_name,
+    email,
+    phone,
+    password,
+    role,
+    hut_name,
+    hut_number,
+    create_at,
+    last_update,
+  }: {
+    UUID?: string;
+    display_name: string;
+    email: string;
+    phone?: string;
+    password?: string;
+    role: number;
+    hut_name?: string;
+    hut_number?: string;
+    create_at?: Date;
+    last_update?: Date;
+  }) {
+    this.UUID = UUID;
+    this.display_name = display_name;
+    this.email = email;
+    this.phone = phone;
+    this.password = password;
+    this.role = role;
+    this.hut_name = hut_name;
+    this.hut_number = hut_number;
+    this.create_at = create_at;
+    this.last_update = last_update;
+  }
+
+  static fromUserContext(userRecord: UserRecord): RegisterRequest {
+    return new RegisterRequest({
+      UUID: userRecord.uid,
+      display_name: userRecord.displayName!,
+      email: userRecord.email!,
+      phone: userRecord.phoneNumber,
+      role: 1, // TOD need an update (ask user)
+    });
+  }
+}
