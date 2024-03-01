487849ea267f155b97a4a5eb8973a993c7d1a20b
Robin <robin59530@gmail.com>
2024-02-29 20:19:54 -0500

    Ajout de variable disable pour les champs provenue par uns service tiers

--- Hunt/model/SignUpForm.ts
+++ Hunt/model/SignUpForm.ts
@@ -13,23 +13,27 @@ export interface ISignUpForm {
   emailTouched?: boolean;
   emailError?: string;
   isEmailValid?: boolean;
+  emailDisable?: boolean;
 
   phone?: string;
   phoneTouched?: boolean;
   phoneError?: string;
   isPhoneValid?: boolean;
+  phoneDisable?: boolean;
 
   password?: string;
   passwordTouched?: boolean;
   passwordError?: string;
   isPasswordValid?: boolean;
   hiddePassword?: boolean;
+  passwordDisable?: boolean;
 
   confirmPassword?: string;
   confirmPasswordTouched?: boolean;
   confirmPasswordError?: string;
   isConfirmPasswordValid?: boolean;
   hiddeConfirmPassword?: boolean;
+  confirmPasswordDisable?: boolean;
 
   hutName?: string;
   hutNameTouched?: boolean;
@@ -46,39 +50,43 @@ export interface ISignUpForm {
 }
 
 export class SignUpForm implements ISignUpForm {
-  UUID?: string = "";
-  fullName: string | undefined = "";
-  fullNameTouched: boolean | undefined = false;
-  fullNameError: string | undefined = "";
-  isFullNameValid: boolean | undefined = false;
-  email: string = "";
-  emailTouched: boolean = false;
-  emailError: string = "";
-  isEmailValid: boolean = false;
-  phone: string | undefined = "";
-  phoneTouched: boolean | undefined = false;
-  phoneError: string | undefined = "";
-  isPhoneValid: boolean | undefined = false;
-  password: string = "";
-  passwordTouched: boolean = false;
-  passwordError: string = "";
-  isPasswordValid: boolean = false;
-  hiddePassword: boolean = false;
-  confirmPassword: string | undefined = "";
-  confirmPasswordTouched: boolean | undefined = false;
-  confirmPasswordError: string | undefined = "";
-  isConfirmPasswordValid: boolean | undefined = false;
-  hiddeConfirmPassword: boolean | undefined = false;
-  hutName: string | undefined = "";
-  hutNameTouched: boolean | undefined = false;
-  hutNameError: string | undefined = "";
-  isHutNameValid: boolean | undefined = false;
-  hutNumber: string | undefined = "";
-  hutNumberTouched: boolean | undefined = false;
-  hutNumberError: string | undefined = "";
-  isHutNumberValid: boolean | undefined = false;
-  isOwner: boolean | undefined = false;
-  isValidForm: boolean = false;
+  UUID?: string;
+  fullName: string;
+  fullNameTouched: boolean;
+  fullNameError: string;
+  isFullNameValid: boolean;
+  email: string;
+  emailTouched: boolean;
+  emailError: string;
+  isEmailValid: boolean;
+  emailDisable: boolean;
+  phone: string;
+  phoneTouched: boolean;
+  phoneError: string;
+  isPhoneValid: boolean;
+  phoneDisable: boolean;
+  password: string;
+  passwordTouched: boolean;
+  passwordError: string;
+  isPasswordValid: boolean;
+  hiddePassword: boolean;
+  passwordDisable: boolean;
+  confirmPassword: string;
+  confirmPasswordTouched: boolean;
+  confirmPasswordError: string;
+  isConfirmPasswordValid: boolean;
+  hiddeConfirmPassword: boolean;
+  confirmPasswordDisable: boolean;
+  hutName: string;
+  hutNameTouched: boolean;
+  hutNameError: string;
+  isHutNameValid: boolean;
+  hutNumber: string;
+  hutNumberTouched: boolean;
+  hutNumberError: string;
+  isHutNumberValid: boolean;
+  isOwner: boolean;
+  isValidForm: boolean;
 
   constructor(
     UUID?: string,
@@ -98,21 +106,25 @@ export class SignUpForm implements ISignUpForm {
     this.email = email || "";
     this.emailTouched = this.email != "" ? true : false;
     this.emailError = "";
+    this.emailDisable = this.email != "" ? false : true;
     this.isEmailValid = this.email != "" ? true : false;
     this.phone = phone || "";
     this.phoneTouched = this.phone != "" ? true : false;
     this.phoneError = "";
     this.isPhoneValid = this.phone != "" ? true : false;
+    this.phoneDisable = this.phone != "" ? false : true;
     this.password = password || "";
     this.passwordTouched = this.password != "" ? true : false;
     this.passwordError = "";
     this.isPasswordValid = this.password != "" ? true : false;
     this.hiddePassword = true;
+    this.passwordDisable = this.password != "" ? false : true;
     this.confirmPassword = confirmPassword || "";
     this.confirmPasswordTouched = this.confirmPassword != "" ? true : false;
     this.confirmPasswordError = "";
     this.isConfirmPasswordValid = this.confirmPassword != "" ? true : false;
     this.hiddeConfirmPassword = true;
+    this.confirmPasswordDisable = this.confirmPassword != "" ? false : true;
     this.hutName = hutName || "";
     this.hutNameTouched = this.hutName != "" ? true : false;
     this.hutNameError = "";
