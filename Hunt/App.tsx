128e4fb7a48a23beb8ca6bcf89c36248c32412b1
Robin <robin59530@gmail.com>
2024-03-02 12:14:06 -0500

    Ajout provider de redux et du RootNavigation

--- Hunt/App.tsx
+++ Hunt/App.tsx
@@ -1,31 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import SignInScreen from "./view/sign/signIn/SignInScreen";
-import SignUpScreen from "./view/sign/signUp/SignUpScreen";
-import AuthProvider from "./utils/provider/authProvider";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { registerRootComponent } from "expo";
-
-const Stack = createNativeStackNavigator();
+import RootNavigation from "./routes/RootNavigation";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="SignIn" component={SignInScreen} />
-          <Stack.Screen name="SignUp" component={SignUpScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthProvider>
+    <Provider store={store}>
+      <RootNavigation />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
 registerRootComponent(App);
