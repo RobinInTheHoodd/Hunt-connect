31b8466b8cc0cf17e7f06d8085406551fef1aaeb
Robin <robin59530@gmail.com>
2024-03-15 11:32:02 -0400

    Ajout de service (get add finish) / controller pour la session de chassre actuelle

---
+++ Hunt/service/huntingSessionContrller.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { IHuntingSessionModel } from "../model/HuntingSession";
+
+export const HuntingSessionController = {
+  async saveHuntSession(huntSession: IHuntingSessionModel) {
+    return await axios.post(
+      "http://10.0.0.228:3000/hunt/session/add",
+      huntSession
+    );
+  },
+
+  async getCurrentHuntSession(userID: string) {
+    return await axios.get(
+      "http://10.0.0.228:3000/hunt/session/getCurrent/" + userID
+    );
+  },
+
+  async finishHuntSession(huntSessionID: number) {
+    console.log(typeof huntSessionID);
+    return await axios.get(
+      "http://10.0.0.228:3000/hunt/session/finish/" + huntSessionID
+    );
+  },
+};
---
+++ Hunt/service/huntingSessionService.ts
@@ -0,0 +1,75 @@
+import { IDuckTeamsModel } from "../model/DuckTeamsModel";
+import HuntingParticipantModel, {
+  IHuntingParticipanModel,
+} from "../model/HuntingParticipantModel";
+import HuntingSessionModel, {
+  IHuntingSessionModel,
+} from "../model/HuntingSession";
+import {
+  IParticipantFormModel,
+  IParticipantModel,
+  ParticipantFormModel,
+} from "../model/ParticipantFormModel";
+import WeatherInfoModel, { IWeatherInfoModel } from "../model/WeatherModel";
+
+import { IWeatherFormModel } from "../model/form/WeatherFormModel";
+import { useAppSelector } from "../redux/hook";
+import { HuntingSessionController } from "./huntingSessionContrller";
+
+export default class HuntingSessionService {
+  async saveHuntingSession(
+    creatorID: string,
+    weatherForm: IWeatherFormModel,
+    duckTeams: IDuckTeamsModel[],
+    participantsForm: IParticipantFormModel
+  ) {
+    try {
+      let toDate = new Date();
+      toDate.setDate(toDate.getDate() + 1);
+
+      const weather: IWeatherInfoModel =
+        WeatherInfoModel.fromWeatherForm(weatherForm);
+
+      const participants: IHuntingParticipanModel[] =
+        HuntingParticipantModel.fromFormModel(participantsForm);
+      const huntSession = new HuntingSessionModel(
+        undefined,
+        undefined,
+        creatorID,
+        new Date(),
+        toDate,
+        false,
+        weather,
+        participants,
+        duckTeams
+      );
+      const result = await HuntingSessionController.saveHuntSession(
+        huntSession
+      );
+
+      huntSession.id = result.data.id;
+      return huntSession;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async finishHuntingSessin(huntingSessionId: number) {
+    try {
+      await HuntingSessionController.finishHuntSession(huntingSessionId);
+    } catch (e: any) {
+      console.log(e);
+    }
+  }
+
+  async getCurrentHuntingSession(userID: string) {
+    try {
+      const res = await HuntingSessionController.getCurrentHuntSession(userID);
+      console.log(res.data);
+      const huntSession: IHuntingSessionModel = res.data;
+      return huntSession;
+    } catch (e: any) {
+      console.log(e);
+    }
+  }
+}
