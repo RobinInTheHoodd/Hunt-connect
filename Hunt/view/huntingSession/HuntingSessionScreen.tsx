9fdb798b82d601b364f11a4ce2a5e54a5c107be0
Robin <robin59530@gmail.com>
2024-03-15 11:06:35 -0400

    Ajout de la page de creation ou vue de la session chasse actuelle

---
+++ Hunt/view/huntingSession/HuntingSessionScreen.tsx
@@ -0,0 +1,47 @@
+import { useCallback, useEffect, useState } from "react";
+import HuntingSessionService from "../../service/huntingSessionService";
+import { IHuntingSessionModel } from "../../model/HuntingSession";
+import WeatherCard from "../home/WeatherCard";
+import AddindHuntingSession from "../../components/AddingHuntingSession";
+import { useAppSelector } from "../../redux/hook";
+import { useFocusEffect } from "@react-navigation/native";
+
+export default function HuntingSessionScreen({ navigation }: any) {
+  const [huntSession, setHuntSession] = useState<IHuntingSessionModel>();
+  const [isLoading, setIsLoading] = useState(true);
+  const user = useAppSelector((state) => state.users);
+  const huntingSessionService = new HuntingSessionService();
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetch = async () => {
+        setIsLoading(true);
+
+        const huntSession =
+          await huntingSessionService.getCurrentHuntingSession(user.UIID);
+        setHuntSession(huntSession);
+        setIsLoading(false);
+      };
+
+      fetch();
+    }, [])
+  );
+
+  useEffect(() => {}, [huntSession, user]);
+
+  return (
+    <>
+      {isLoading ? (
+        <></>
+      ) : (
+        <>
+          {huntSession ? (
+            <WeatherCard HuntSession={huntSession} navigation={navigation} />
+          ) : (
+            <AddindHuntingSession SetHunting={setHuntSession} />
+          )}
+        </>
+      )}
+    </>
+  );
+}
