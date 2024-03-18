0850a92d7d6ee38470a68f9ecee898b5aaedab4f
Robin <robin59530@gmail.com>
2024-03-18 16:20:28 -0400

    Modificaiton APP

--- Hunt/App.tsx
+++ Hunt/App.tsx
@@ -2,17 +2,14 @@ import store from "./redux/store";
 import { Provider } from "react-redux";
 import RootNavigation from "./routes/RootNavigation";
 import { registerRootComponent } from "expo";
-import Home from "./view/home/HomeScreen";
-
-import AddindHuntingSession from "./components/AddingHuntingSession";
-import LottieView from "lottie-react-native";
-import MapView from "react-native-maps";
-import HuntingSessionScreen from "./view/huntingSession/HuntingSessionScreen";
+import { LoadingVisibilityProvider } from "./utils/LoadingVisibilityContext";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <RootNavigation />
+      <LoadingVisibilityProvider>
+        <RootNavigation />
+      </LoadingVisibilityProvider>
     </Provider>
   );
 }
