b07f487bb7015c85a5ee950ee7bdccd11aa10d70
Robin <robin59530@gmail.com>
2024-03-18 16:17:02 -0400

    Modification du routage pour hunt session

--- Hunt/routes/RootNavigation.tsx
+++ Hunt/routes/RootNavigation.tsx
@@ -6,69 +6,71 @@ import React, { useEffect } from "react";
 import { useAppSelector } from "../redux/hook";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../view/home/HomeScreen";
-import WeatherScreen from "../components/HuntingSessionInfo";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faDove, faLocation } from "@fortawesome/free-solid-svg-icons";
-
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import HuntingSessionScreen from "../view/huntingSession/HuntingSessionScreen";
+import { useLoadingVisibility } from "../utils/LoadingVisibilityContext";
+import HuntingScreen from "../view/huntingSession/HuntingScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function RootNavigation() {
   const user = useAppSelector((state) => state.users);
+  const { isLoadingVisible, setLoadingVisible } = useLoadingVisibility();
   useEffect(() => {
     console.log(user);
   }, [user]);
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="SignIn"
-          screenOptions={{ headerShown: false }}
-        >
-          {user.isNew ? (
-            <>
-              <Stack.Screen name="SignIn" component={SignInScreen} />
-              <Stack.Screen name="SignUp" component={SignUpScreen} />
-            </>
-          ) : (
-            <Stack.Screen name="Dashboard">
-              {() => (
-                <Tab.Navigator
-                  initialRouteName="Home"
-                  screenOptions={({ route }) => ({
-                    headerShown: false,
-                    tabBarIcon: ({ focused, color, size }) => {
-                      let iconName;
-                      // You can return any component that you like here!
-                      return (
-                        <FontAwesomeIcon
-                          icon={faDove}
-                          size={size}
-                          color={color}
-                        />
-                      );
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SignIn"
+        screenOptions={{ headerShown: false }}
+      >
+        {user.isNew ? (
+          <>
+            <Stack.Screen name="SignIn" component={SignInScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+          </>
+        ) : (
+          <Stack.Screen name="Dashboard">
+            {() => (
+              <Tab.Navigator
+                initialRouteName="Home"
+                screenOptions={({ route }) => ({
+                  headerShown: false,
+                  tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    return (
+                      <FontAwesomeIcon
+                        icon={faDove}
+                        size={size}
+                        color={color}
+                      />
+                    );
+                  },
+                  tabBarActiveTintColor: "tomato",
+                  tabBarInactiveTintColor: "gray",
+                })}
+              >
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen
+                  name="Session de chasse"
+                  component={HuntingScreen}
+                  listeners={{
+                    tabPress: (e) => {
+                      setLoadingVisible(true);
                     },
-                    tabBarActiveTintColor: "tomato",
-                    tabBarInactiveTintColor: "gray",
-                  })}
-                >
-                  <Tab.Screen name="Home" component={HomeScreen} />
-                  <Tab.Screen
-                    name="Session de chasse"
-                    component={HuntingSessionScreen}
-                    options={{ tabBarStyle: { display: "none" } }}
-                  />
-                  <Tab.Screen name="Settings" component={HomeScreen} />
-                </Tab.Navigator>
-              )}
-            </Stack.Screen>
-          )}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+                  }}
+                  options={{ tabBarStyle: { display: "none" } }}
+                />
+                <Tab.Screen name="Settings" component={HomeScreen} />
+              </Tab.Navigator>
+            )}
+          </Stack.Screen>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
