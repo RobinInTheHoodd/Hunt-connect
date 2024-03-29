0bcbebad31b84b1d0b4338c1ed48250c3fb5b878
Robin <robin59530@gmail.com>
2024-03-29 12:04:16 -0400

    Ajout de reduce du observationModel

--- Hunt/redux/store.ts
+++ Hunt/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/userSlice";
+import observationReducer from "./reducers/observationSlice";
 
 const store = configureStore({
   reducer: {
     users: userReducer,
+    observations: observationReducer,
   },
 });
 
