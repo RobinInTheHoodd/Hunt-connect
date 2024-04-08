a706ab5b5a9d3f2d162b5ecb2ed051e880edec66
Robin <robin59530@gmail.com>
2024-04-08 17:13:12 -0400

    update app

--- Hunt/App.tsx
+++ Hunt/App.tsx
@@ -1,13 +1,11 @@
 import store from "./redux/store";
+
 import { Provider } from "react-redux";
 import RootNavigation from "./routes/RootNavigation";
 import { registerRootComponent } from "expo";
 import { LoadingVisibilityProvider } from "./utils/LoadingVisibilityContext";
-import { useState } from "react";
-import { IParticipantModel } from "./model/ParticipantFormModel";
 
 export default function App() {
-  const [form, setForm] = useState<IParticipantModel[]>([]);
   return (
     <Provider store={store}>
       <LoadingVisibilityProvider>
